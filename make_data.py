447cd837a04f82078e74338e57582f9f53ea4952
Kibum Bing <110390520+kibumbing@users.noreply.github.com>
2023-05-10 15:28:06 +0900

    Add files via upload

--- make_data.py
+++ make_data.py
@@ -1,4 +1,4 @@
 import SoccerNet
 from SoccerNet.Downloader import SoccerNetDownloader
-mySoccerNetDownloader=SoccerNetDownloader()
+mySoccerNetDownloader=SoccerNetDownloader(LocalDirectory="")
 mySoccerNetDownloader.downloadDataTask(task="jersey-2023", split=["train","test","challenge"])
